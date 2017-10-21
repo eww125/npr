fbf70f3e800afd37118fdf51eba4d35841e02967
Evan Wappel <eww125@evans-mbp.attlocal.net>
2017-10-21 16:41:54 -0700

    new files

---
+++ date_teaser_byline.py
@@ -0,0 +1,27 @@
+from urllib2 import urlopen
+from json import load, dumps
+
+# api_key import
+import os
+home_dir =  os.path.expanduser('~')
+directory_path = home_dir + "/api_keys/"
+f = open(directory_path + 'npr_api', 'r')
+API_KEY = f.read().rstrip()
+f.close()
+
+url = 'http://api.npr.org/query?apiKey='
+key = API_KEY
+url = url + key
+url += '&numResults=3&format=json&id=1007'  # 1007 is science
+
+response = urlopen(url)
+json_obj = load(response)
+
+# uncomment 3 lines below to see JSON output to file
+f = open('output.json', 'w')
+f.write(dumps(json_obj, indent=4))
+f.close()
+
+for story in json_obj['list']['story']:
+    print "TITLE: " + story['title']['$text'] + "\n"
+    print story['byline']
--- story_titles.py
+++ story_titles.py
@@ -1,17 +1,21 @@
 from urllib2 import urlopen
 from json import load
 
+# api_key import
+import os
+home_dir =  os.path.expanduser('~')
+directory_path = home_dir + "/api_keys/"
+f = open(directory_path + 'npr_api', 'r')
+API_KEY = f.read().rstrip()
+f.close()
+
 url = 'http://api.npr.org/query?apiKey='
-key = 'MDAxNzgzOTYyMDEyMTUwMDkyMDM1ZWUwYw001'
-url = url + key
+url = url + API_KEY
 url += '&numResults=3&format=json&id=1001'
 #url += raw_input("Which NPR ID do you want to query?")
-print url
 
 response = urlopen(url)
 json_obj = load(response)
 
 for story in json_obj['list']['story']:
 	print story['title']['$text']
-
-#MDAxNzgzOTYyMDEyMTUwMDkyMDM1ZWUwYw001
