626b457cc9c6e8bd70dbf87df22a5244f03ee7ed
Evan Wappel <ewappel@ewappel-macbookpro.roam.corp.google.com>
2018-04-26 08:44:22 -0700

    cleanup

--- placekitten.py
+++
@@ -1,5 +0,0 @@
-from urllib2 import urlopen
-website = urlopen("http://placekitten.com")
-#website = urlopen("https://raw.githubusercontent.com/eww125/ascii_art/master/eeyore")
-kittens = website.read()
-print kittens[0:1200]
