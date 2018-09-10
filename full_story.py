bec9c53ae5b0879fab54b6d51705498de6d251e7
Evan Wappel <ewappel@gmail.com>
2018-09-10 16:48:39 -0700

    py3

--- full_story.py
+++ full_story.py
@@ -1,11 +1,11 @@
-from urllib2 import urlopen
+import urllib.request
 from json import load
 
 # api_key import
 import os
 home_dir =  os.path.expanduser('~')
 directory_path = home_dir + "/api_keys/"
-f = open(directory_path + 'npr_api', 'r')
+f = open(directory_path + 'npr_api_key', 'r')
 API_KEY = f.read().rstrip()
 f.close()
 
@@ -17,33 +17,33 @@ url += '&numResults=1&format=json&id=1007'
 url += '&requiredAssets=text,image,audio'
 
 # open our url, load the JSON
-response = urlopen(url)
+response = urllib.request.urlopen(url)
 json_obj = load(response)
 
 # parse our story
 for story in json_obj['list']['story']:
-    print "TITLE: " + story['title']['$text'] + "\n"
-    print "DATE: " + story['storyDate']['$text'] + "\n"
-    print "TEASER:" + story['teaser']['$text'] + "\n"
+    print("TITLE: " + story['title']['$text'] + "\n")
+    print("DATE: " + story['storyDate']['$text'] + "\n")
+    print("TEASER:" + story['teaser']['$text'] + "\n")
 
     if 'byline' in story:
-        print "BYLINE: " + story['byline'][0]['name']['$text'] + "\n"
+        print("BYLINE: " + story['byline'][0]['name']['$text'] + "\n")
 
     if 'show' in story:
-        print "PROGRAM: " + story['show'][0]['program']['$text'] + "\n"
+        print("PROGRAM: " + story['show'][0]['program']['$text'] + "\n")
 
-    print "NPR URL: " + story['link'][0]['$text'] + "\n"
-    print "IMAGE: " + story['image'][0]['src'] + "\n"
+    print("NPR URL: " + story['link'][0]['$text'] + "\n")
+    print("IMAGE: " + story['image'][0]['src'] + "\n")
 
     if 'caption' in story:
-        print "IMAGE CAPTION: ", story['image'][0]['caption']['$text'] + "\n"
+        print("IMAGE CAPTION: ", story['image'][0]['caption']['$text'] + "\n")
 
     if 'producer' in story:
-        print "IMAGE CREDIT: " + story['image'][0]['producer']['$text'] + "\n"
+        print("IMAGE CREDIT: " + story['image'][0]['producer']['$text'] + "\n")
 
-    print "MP3 AUDIO: " + story['audio'][0]['format']['mp3'][0]['$text'] + "\n"
+    print("MP3 AUDIO: " + story['audio'][0]['format']['mp3'][0]['$text'] + "\n")
 
     # loop through and print each paragraph
     for paragraph in story['textWithHtml']['paragraph']:
-        print paragraph['$text'] + " \n"
+        print(paragraph['$text'] + " \n")
 
